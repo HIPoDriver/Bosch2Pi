07ae02d0b79c2b8d4bda73527890987d5972aebb
HiPo Driver <info@hipodriver.com>
2025-08-09 10:35:53 -0400

    Add project files.

---
+++ Bosch2Pi/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bosch2Pi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check argumants and open files
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: Bosch2Pi <data_file>");
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("File not found: " + args[0]);
+                return;
+            }
+
+            string infile = args[0];
+            string outfile = args[0] + ".pi.txt";
+
+
+            // Read all lines from the CSV file
+            var lines = File.ReadAllLines(infile)
+                .Where(line => !line.TrimStart().StartsWith("#"))
+                .ToArray();
+
+            // Split each line into fields
+            var rows = lines.Select(line => line.Split('\t')).ToList();
+
+            // Transpose rows to columns
+            int columnCount = rows[0].Length;
+            var columns = new List<List<string>>();
+
+            for (int col = 0; col < columnCount; col++)
+            {
+                var column = new List<string>();
+                foreach (var row in rows)
+                {
+                    column.Add(row[col]);
+                }
+                columns.Add(column);
+            }
+
+            // Write the Pi file header
+            var writer = new StreamWriter(outfile, false, Encoding.GetEncoding(1252));      //Important to use the 1252 encoding to match Pi Toolbox ASCII format      
+
+            // File header
+            writer.WriteLine("PiToolboxVersionedASCIIDataSet");
+            writer.WriteLine("Version\t2");
+            writer.WriteLine();
+            writer.WriteLine("{OutingInformation}");
+            writer.WriteLine($"CarName\tBosch");
+            writer.WriteLine("FirstLapNumber\t0");
+
+            // Cycle through and create channel blocks
+            for (int i = 1; i < columns.Count; i++)
+            {
+                writer.WriteLine();
+                writer.WriteLine("{ChannelBlock}");
+                writer.WriteLine($"Time\t{columns[i][0]}");
+
+                for (int j = 1; j < columns[i].Count; j++)
+                {
+                    if (double.TryParse(columns[0][j], out double value))
+                    {
+                        writer.WriteLine($"{value}\t{columns[i][j]}");
+                    }
+                    else
+                    {
+                        // Handle non-numeric data (optional)
+                        writer.WriteLine($"{columns[0][j]}\t{columns[i][j]}");
+                    }
+
+                }
+            }
+
+            writer.Close();
+        }
+    }
+}
---
+++ Bosch2Pi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bosch2Pi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bosch2Pi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88e78afa-24d0-4bfb-a3ee-d1900f0165d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
